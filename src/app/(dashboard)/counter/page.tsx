eb7bd01c7b3d907ab5d23b7a68c013b9683c1481
AndDev <23031429@itcelaya.edu.mx>
2025-03-27 21:03:35 -0600

    Remove unused metadata from CounterPage component

--- src/app/(dashboard)/counter/page.tsx
+++ src/app/(dashboard)/counter/page.tsx
@@ -1,11 +1,7 @@
 import { Counter } from "@/components/Counter";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import React from "react";
 
-export const metadata: Metadata = {
-  title: "CastDev-j | Contador",
-  description: "CastDev-j | Contador",
-};
 
 export const CounterPage = () => {
   return (
