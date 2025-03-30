941738545a84506229f065a99090c34fbf735b9c
AndDev <23031429@itcelaya.edu.mx>
2025-03-29 18:11:59 -0600

    generic interpretation used for better use

--- src/app/(dashboard)/pokemons/helpers/getPokemon.ts
+++ src/app/(dashboard)/pokemons/helpers/getPokemon.ts
@@ -1,10 +1,13 @@
 import { PokemonDetails } from "@/interfaces/pokemons/PokemonDetails";
 import { notFound } from "next/navigation";
 
-export const getPokemon = async (id: number) => {
+export const getPokemon = async (q: number | string) => {
   try {
-    const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-      cache: "force-cache", //Todo cambiar despues
+    const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${q}`, {
+      // cache: "force-cache", // hace que la peticion no se vuelva a hacer si ya existe en cache
+      next: {
+        revalidate: 60 * 60 * 24 * 30, // 30 dias
+      },
     }).then<PokemonDetails>((res) => res.json());
 
     return {
